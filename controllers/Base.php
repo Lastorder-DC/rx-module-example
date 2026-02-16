4b40e1561e5837291229960ebee56cef742ba6a4
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2026-02-17 07:59:00 +0900

    모듈 생성기로 만든 파일 업로드
    https://www.poesis.dev/tools/rxmodulegen

---
+++ controllers/Base.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rhymix\Modules\Module_example\Controllers;
+
+/**
+ * 라이믹스 모듈 예제
+ * 
+ * Copyright (c) Developer
+ * 
+ * Generated with https://www.poesis.dev/tools/rxmodulegen
+ */
+class Base extends \ModuleObject
+{
+	
+}
---
+++ lang/ko.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang->cmd_module_example = '라이믹스 모듈 예제';
+$lang->cmd_module_example_general_config = '기본 설정';
+$lang->cmd_module_example_example_config = '예제 설정';
